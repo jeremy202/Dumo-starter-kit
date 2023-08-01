ca9c83f21e469f88e385b39bef03f4893426c50a
jerry <jeremiahovundah@gmail.com>
2023-08-01 20:58:01 +0100

    jerry

--- modules/dumo-ui/components/DumoDropdownBig.vue
+++ modules/dumo-ui/components/DumoDropdownBig.vue
@@ -11,8 +11,11 @@
     </div>
 
     <div v-click-outside="close" v-if="showDropdown" class="absolute dropdown-menu text-x-small">
-      <span v-for="(item, idx) in items" :key="idx" class="flex items-center gap-2 capitalize cursor-pointer"
+      <span v-for="(item, idx) in items" :key="idx" class="flex items-center gap-2 capitalize cursor-pointer drop-conten-padding"
         @click="selectItem(item)">
+        <span v-if="item.image">
+          <img :src="item.image" alt="" />
+        </span>
         {{ item.text }}
       </span>
     </div>
@@ -23,7 +26,7 @@
 const props = defineProps<{
   modelValue: { text: string; value: any, image?: string };
   placeholder?: string;
-  items?: { text: string; value: any, image?: string }[];
+  items?: { text: string; value: any };
   width?: string;
   background?: string;
   color?: string;
@@ -56,8 +59,8 @@ const selectItem = (item: any) => {
 <style scoped>
 .dropdown-select {
   width: 100%;
-  border-radius: 10px;
-  padding: 6px 10px;
+  border-radius: 14px;
+  padding: 14px 16px;
   border: 2px solid #E7EAF4;
   color: #4B4B4D;
   background-color: #FCFCFC;
@@ -65,11 +68,17 @@ const selectItem = (item: any) => {
 
 .dropdown-menu {
   width: 100%;
-  border-radius: 10px;
-  padding: 6px;
+  margin-top: 4px;
+  padding: 10px 0;
+  border-radius: 4px 4px 14px 14px;
   border: 2px solid #E7EAF4;
   color: #4B4B4D;
-  background-color: #FCFCFC;
+  background-color: #EBEBEB;
+  position: absolute;
+  z-index: 2;
+}
+.drop-conten-padding {
+  padding: 10px 16px;
 }
 
 .icon {
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,36 @@
 <template>
-  <div class="mx-auto text-2xl font-extrabold">
-    <h1>DUMO PROJECT STARTER TEMPLATE</h1>
+  <div class="flex flex-col justify-center">
+    <div class="mx-auto text-2xl font-extrabold">
+      <h1>DUMO PROJECT STARTER TEMPLATE</h1>
+    </div>
+
+    <div class="mx-auto mt-5">
+      <DumoButton cta>
+        DUMO
+      </DumoButton>
+    </div>
+
+    <div class="mx-auto mt-5">
+      <DumoTextArea v-model="form.password" :label="form.password ? '' : 'Text area'"
+        :prepend="form.password ? 'Text area' : ''"></DumoTextArea>
+    </div>
+    <div class="max-w-xs mt-5">
+      <DumoDropdownBig placeholder="- Select Organisation -" :items="organisation" />
+    </div>
+    <div class="max-w-xs mt-5">
+      <DumoInputStat placeholder="Select Organisation"  />
+    </div>
+
+    <div class="flex flex-col justify-center ml-60">
+      <div class="max-w-xs mt-5">
+        <dumo-select label="- Select Organisation -" :items="organisation"></dumo-select>
+      </div>
+      <div class="max-w-xs mt-5">
+        <dumo-select v-model="form.country" required>
+          <option value disabled selected>— Country —</option>
+        </dumo-select>
+      </div>
+    </div>
   </div>
 
   <div class="mx-auto mt-5">
@@ -26,12 +56,20 @@
 
 <script setup lang="ts">
 const snackbar = useSnackbar();
-
 const form = reactive({
   email: "",
   password: "",
   country: ""
 });
+
+const organisation = [{
+  text: 'Chigisoft',
+  value: 'chigisoft'
+},
+{
+  text: 'DUMO Ava',
+  value: 'ava'
+}]
 </script>
 
 <style scoped></style>
