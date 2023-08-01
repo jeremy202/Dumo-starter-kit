9aae4189ddf5458722a27c8d598a43ef9e519ecd
Joshua <afekurojoshua@gmail.com>
2023-08-01 12:19:53 +0100

    feat: dumo button link component

---
+++ modules/dumo-ui/components/DumoButtonLink.vue
@@ -0,0 +1,40 @@
+<template>
+  <NuxtLink :to="route" class="button-link" :class="classes" :style="styles" v-bind="$attrs">
+    <slot></slot>
+    <img v-show="image" :src="image" alt="Image" />
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  color?: string;
+  image?: string,
+  success?: boolean
+  route?: string
+}>();
+
+const styles = {
+  ...(props.color && { color: props.color }),
+};
+
+const classes = {
+  ['success']: props.success
+}
+</script>
+
+<style scoped>
+.button-link {
+  color: var(--main-light);
+  font-weight: 600;
+  font-size: 12px;
+  text-transform: uppercase;
+  letter-spacing: 0.72px;
+  display: inline-flex;
+  align-items: center;
+  gap: 8px;
+}
+
+.button-link.success {
+  color: var(--primary-green);
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,49 +1,22 @@
 <template>
-  <div class="flex flex-col justify-center">
-    <div class="mx-auto text-2xl font-extrabold">
-      <h1>DUMO PROJECT STARTER TEMPLATE</h1>
-    </div>
+  <div class="mx-auto text-2xl font-extrabold">
+    <h1>DUMO PROJECT STARTER TEMPLATE</h1>
+  </div>
 
-    <div class="mx-auto mt-5">
-      <DumoButton cta>
-        DUMO
-      </DumoButton>
-    </div>
-
-    <!-- <div class="mx-auto mt-5">
-      <DumoTextArea v-model="form.password" :label="form.password ? '' : 'Text area'"
-        :prepend="form.password ? 'Text area' : ''"></DumoTextArea>
-    </div>
-
-    <div class="flex flex-col justify-center ml-60">
-      <div class="max-w-xs mt-5">
-        <dumo-select label="- Select Organisation -" :items="organisation"></dumo-select>
-      </div>
-      <div class="max-w-xs mt-5">
-        <dumo-select v-model="form.country" required>
-          <option value disabled selected>— Country —</option>
-        </dumo-select>
-      </div>
-    </div> -->
+  <div class="mx-auto mt-5">
+    <DumoButton cta>
+      DUMO
+    </DumoButton>
+  </div>
+  <div class="mx-auto mt-5">
+    <DumoButtonLink to="/" image="/assets/images/ic-green-check.svg">
+      DUMO Button link
+    </DumoButtonLink>
   </div>
 </template>
 
 <script setup lang="ts">
 const snackbar = useSnackbar();
-// const form = reactive({
-//   email: "",
-//   password: "",
-//   country: ""
-// });
-
-// const organisation = [{
-//   text: 'Chigisoft',
-//   value: 'chigisoft'
-// },
-// {
-//   text: 'DUMO Ava',
-//   value: 'ava'
-// }]
 </script>
 
 <style scoped></style>
