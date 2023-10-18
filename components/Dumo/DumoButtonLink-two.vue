f36b31ad131a0dda3b7bb572953514f891a64542
jerry <jeremiahovundah@gmail.com>
2023-10-18 17:56:10 +0100

    jerry

---
+++ components/Dumo/DumoButtonLink-two.vue
@@ -0,0 +1,46 @@
+<template>
+  <NuxtLink
+    :to="route"
+    class="button-link"
+    :class="classes"
+    :style="styles"
+    v-bind="$attrs">
+    <slot></slot>
+    <img v-if="image" :src="image" alt="Image" />
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  color?: string
+  image?: string
+  success?: boolean
+  route?: string
+  uppercase?: boolean
+}>()
+
+const styles = {
+  ...(props.color && { color: props.color }),
+  ...(props.uppercase && { textTransform: 'upperCase' }),
+}
+
+const classes = {
+  ['success']: props.success,
+}
+</script>
+
+<style scoped>
+.button-link {
+  color: #1E1F21;
+  font-weight: 600;
+  font-size: 16px;
+  display: inline-flex;
+  align-items: center;
+  gap: 8px;
+  text-decoration: underline;
+}
+
+.button-link.success {
+  color: var(--primary-green);
+}
+</style>
