f10088509db70681e1e3575c71596ac24d55400e
Joshua <afekurojoshua@gmail.com>
2024-03-11 15:38:57 +0100

    chore: prevent negative value for number input

--- components/Dumo/DumoCurrencyDropdown.vue
+++ components/Dumo/DumoCurrencyDropdown.vue
@@ -2,7 +2,8 @@
   <div>
     <div class="flex dumo-currency-drop-container">
       <div class="relative" :style="styles" ref="dropdownRef">
-        <div class="flex items-center justify-between font-medium cursor-pointer dropdown-select-currency small-paragraph"
+        <div
+          class="flex items-center justify-between font-medium cursor-pointer dropdown-select-currency small-paragraph"
           @click="showDropdown = !showDropdown">
           <div class="flex items-center gap-2 uppercase">
             <span v-if="selected">{{ selected.currency }}</span>
@@ -14,42 +15,49 @@
         </div>
 
         <div v-if="showDropdown" class="absolute dropdown-menu small-paragraph">
-          <span v-for="(item, idx) in items" :key="idx"
-            class="flex items-center gap-2 uppercase cursor-pointer drop-content-padding" @click="selectItem(item)">
+          <span
+            v-for="(item, idx) in items"
+            :key="idx"
+            class="flex items-center gap-2 uppercase cursor-pointer drop-content-padding"
+            @click="selectItem(item)">
             {{ item.currency }}
           </span>
         </div>
       </div>
-      <input class="custom-input" type="number" placeholder="0.00" :value="props.modelValue.amount" @input="onChange"
+      <input
+        class="custom-input"
+        type="number"
+        min="0"
+        placeholder="0.00"
+        :value="props.modelValue.amount"
+        @input="onChange"
         v-bind="$attrs" />
     </div>
   </div>
 </template>
 
-
 <script setup lang="ts">
 import { onClickOutside } from '@vueuse/core'
 
 type Item = {
-  currency: string;
-  amount: string;
-};
+  currency: string
+  amount: string
+}
 
 const props = defineProps<{
-  modelValue: Item;
-  placeholder?: string;
-  items?: Item[];
-  width?: string;
-  background?: string;
-  color?: string;
-  border?: string;
-}>();
+  modelValue: Item
+  placeholder?: string
+  items?: Item[]
+  width?: string
+  background?: string
+  color?: string
+  border?: string
+}>()
 
+const $emit = defineEmits(['update:modelValue'])
 
-const $emit = defineEmits(["update:modelValue"]);
-
-const showDropdown = ref(false);
-const selected = ref<Item>(props.modelValue);
+const showDropdown = ref(false)
+const selected = ref<Item>(props.modelValue)
 
 const close = () => {
   showDropdown.value = false
@@ -64,41 +72,43 @@ const styles = {
   ...(props.background && { background: props.background }),
   ...(props.color && { color: props.color }),
   ...(props.border && { border: props.border }),
-};
+}
 
 const selectItem = (item: Item) => {
-  selected.value = item;
-  showDropdown.value = false;
+  selected.value = item
+  showDropdown.value = false
 
-  $emit("update:modelValue", item);
-};
+  $emit('update:modelValue', item)
+}
 
 const onChange = (e: any) => {
-  selected.value.amount = e.target.value;
+  selected.value.amount = e.target.value
 
   const updatedItem = {
     ...selected.value,
     amount: e.target.value,
   }
 
-  $emit("update:modelValue", updatedItem);
-};
+  $emit('update:modelValue', updatedItem)
+}
 </script>
 
 <style scoped>
+
+
 .dropdown-select-currency {
   padding: 10px 8px;
-  background: #E7EAF4;
+  background: #e7eaf4;
   border-radius: 12px;
 }
 
 .dumo-currency-drop-container {
   padding: 4px;
-  border: 1.5px solid #E7EAF4;
+  border: 1.5px solid #e7eaf4;
   border-radius: 14px;
   width: 100%;
   font-size: 16px;
-  color: rgba(30, 31, 33, .8);
+  color: rgba(30, 31, 33, 0.8);
   appearance: none;
   position: relative;
   background-color: transparent;
@@ -132,9 +142,9 @@ const onChange = (e: any) => {
   margin-top: 4px;
   padding: 10px 0;
   border-radius: 4px 4px 14px 14px;
-  border: 2px solid #E7EAF4;
-  color: #4B4B4D;
-  background-color: #EBEBEB;
+  border: 2px solid #e7eaf4;
+  color: #4b4b4d;
+  background-color: #ebebeb;
   position: absolute;
   z-index: 2;
 }
